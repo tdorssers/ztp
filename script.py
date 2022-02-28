43f73a398ccaa60799194a5686bb888f0d6e67ee
tdorssers <tim.dorssers@xs4all.nl>
2022-02-28 11:19:01 +0100

    Fix failed config logging and overall refactoring

--- script.py
+++ script.py
@@ -17,7 +17,6 @@ Version: 1.2
 
 import os
 import re
-import cli
 import sys
 import json
 import time
@@ -28,6 +27,7 @@ try:
     from urlparse import urljoin
 except ImportError:
     from urllib.parse import urljoin
+import cli
 
 ##### CONSTANTS ################################################################
 
@@ -136,7 +136,7 @@ def upload(**kwargs):
                 os.mkdir('/bootflash/guest-share')
             with open('/bootflash/guest-share/temp.json', 'w') as outfile:
                 json.dump(ztp, outfile)
-        except (IOError, ValueError) as e:
+        except (OSError, IOError, ValueError) as e:
             log(3, e)
             return
 
@@ -180,46 +180,47 @@ def renumber_stack(stack, serials):
 
     # get current switch number and priorities as list of tuples
     switch = cli.execute('show switch')
-    match = re.findall('(\d)\s+\S+\s+\S+\s+(\d+)', switch)
+    match = re.findall(r'(\d)\s+\S+\s+\S+\s+(\d+)', switch)
     # renumber switches
     renumber = False
     for old_num in serials:
         # lookup new switch number
         new_num = next((n for n in stack if serials[old_num] == stack[n]), None)
-        if new_num and old_num != int(new_num):
+        if not new_num:
+            continue
+        if old_num != int(new_num):
             renumber = True
             # renumber switch and log error message in case of failure
             try:
                 cli.execute('switch {} renumber {}'.format(old_num, new_num))
                 log(6, 'Renumbered switch {} to {}'.format(old_num, new_num))
-            except Exception as e:
+            except Exception as e:  # broad except to support renames in 16.12.x
                 log(3, e)
                 shutdown(save=False, abnormal=True)  # terminate script
 
-        if new_num:
-            # calculate new switch priority
-            new_prio = 16 - int(new_num)
-            # lookup current switch priority
-            old_prio = next((p for n, p in match if int(n) == old_num), 1)
-            if int(old_prio) != new_prio:
-                # check if top switch is not active
-                if switch.find('*{}'.format(sorted(serials.keys())[0])) == -1:
-                    renumber = True
+        # calculate new switch priority
+        new_prio = 16 - int(new_num)
+        # lookup current switch priority
+        old_prio = next((prio for num, prio in match if int(num) == old_num), 1)
+        if int(old_prio) != new_prio:
+            # check if top switch is not active
+            if switch.find('*{}'.format(sorted(serials.keys())[0])) == -1:
+                renumber = True
 
-                # set switch priority and log error message in case of failure
-                try:
-                    cli.execute('switch %s priority %d' % (old_num, new_prio))
-                    log(6, 'Switch %s priority set to %d' % (old_num, new_prio))
-                except Exception as e:
-                    log(3, e)
-                    shutdown(save=False, abnormal=True)  # terminate script
+            # set switch priority and log error message in case of failure
+            try:
+                cli.execute('switch %s priority %d' % (old_num, new_prio))
+                log(6, 'Switch %s priority set to %d' % (old_num, new_prio))
+            except Exception as e:  # broad except to support renames in 16.12.x
+                log(3, e)
+                shutdown(save=False, abnormal=True)  # terminate script
 
     if renumber:
         for num in serials.keys():
             # to prevent recovery from backup nvram
             try:
                 cli.execute('delete flash-%s:nvram_config*' % num)
-            except Exception as e:
+            except:  # broad except to support class renames in 16.12.x
                 pass
 
     return renumber
@@ -228,8 +229,9 @@ def install(target, is_chassis):
     """ Returns True if install script is configured or False otherwise """
     # remove leading zeros from required version numbers and compare
     if (target.version is None or target.install is None
-        or ztp['version'] == re.sub(r'\b0+(\d)', r'\1', target.version.strip())):
-            return False
+            or ztp['version'] == re.sub(r'\b0+(\d)', r'\1',
+                                        target.version.strip())):
+        return False
 
     install_url = urljoin(target.base_url, target.install)
     # terminate script in case of invalid file
@@ -247,13 +249,13 @@ def install(target, is_chassis):
         cli.execute('write memory')
         cli.execute('write erase')
         # install command needs confirmation on changed boot config
-        confirm_bm = '''pattern "\[y\/n\]|#"
+        confirm_bm = r'''pattern "\[y\/n\]|#"
             action 5.3 cli command "y"'''
     else:
         confirm_bm = ''
 
     # Configure EEM applet for interactive command execution
-    cli.configure('''event manager applet upgrade
+    cli.configure(r'''event manager applet upgrade
         event none maxrun 900
         action 1.0 cli command "enable"
         action 2.0 syslog msg "Removing inactive images..."
@@ -280,12 +282,11 @@ def autoupgrade():
             action 3.0 syslog msg "Reloading stack..."
             action 4.0 reload''')
         return True
-    else:
-        return False
+    return False
 
 def parse_hex(fmt):
     """ Converts the hex/text format of the IOS more command to string """
-    match = re.findall('\S{8}: +(\S{8} +\S{8} +\S{8} +\S{8})', fmt)
+    match = re.findall(r'\S{8}: +(\S{8} +\S{8} +\S{8} +\S{8})', fmt)
     parts = [base64.b16decode(re.sub(' |X', '', line)) for line in match]
     return ''.join(parts) if match else fmt
 
@@ -305,17 +306,16 @@ def download(file_url):
         # extract file contents from output
         match = re.search('^Loading %s (.*)' % file_url, result, re.DOTALL)
         return parse_hex(match.group(1)) if match else ''
-    else:
-        return ''
+    return ''
 
 def apply_config(target):
     """ Returns True if configuration template is applied successfully """
     cfg_url = urljoin(target.base_url, target.config) if target.config else None
 
     # remove keyword 'end' from downloaded configuration
-    conf = re.sub('^\s*end\s*$', '', download(cfg_url), flags=re.MULTILINE)
+    conf = re.sub(r'^\s*end\s*$', '', download(cfg_url), flags=re.MULTILINE)
     if target.template:
-        conf += '\n' + target.template if len(conf) else target.template
+        conf += '\n' + target.template if conf else target.template
 
     if len(conf) == 0:
         return False
@@ -327,8 +327,8 @@ def apply_config(target):
     # apply configuration and log error message in case of failure
     try:
         cli.configure(conf)
-    except Exception as e:
-        log(3, 'Failed configurations:\n' + '\n'.join(map(str, e.failed)))
+    except Exception as e:  # broad except to support class renames in 16.12.x
+        log(3, e)
         shutdown(save=False, abnormal=True)  # terminate script
     else:
         return True
@@ -339,12 +339,12 @@ def blue_beacon(sw_nums):
         # up to and including 16.8.x
         try:
             cli.cli('configure terminal ; hw-module beacon on switch %d' % num)
-        except:
+        except:  # broad except to support class renames in 16.12.x
             pass
         # from 16.9.x onwards
         try:
             cli.execute('hw-module beacon slot %d on' % num)
-        except:
+        except:  # broad except to support class renames in 16.12.x
             pass
 
         log(6, 'Switch %d beacon LED turned on' % num)
@@ -373,7 +373,7 @@ def final_cli(command):
 
             try:
                 output = cli.execute(cmd)  # execute command
-            except Exception as e:
+            except Exception as e:  # broad except to support renames in 16.12.x
                 log(3, 'Final command failure: %s' % e)
                 success = False
             else:
@@ -384,6 +384,7 @@ def final_cli(command):
     return success
 
 def main():
+    """ Executes main workflow """
     # setup IOS syslog for our own messages if server IP is specified
     if SYSLOG:
         cli.configure('''logging discriminator ztp msg-body includes Message from|HA_EM|INSTALL
@@ -400,7 +401,7 @@ def main():
     # load JSON formatted data if URL is specified and concatenate it to DATA
     json_str = download(JSON)
     try:
-        data = DATA + json.loads(json_str) if len(json_str) else DATA
+        data = DATA + json.loads(json_str) if json_str else DATA
     except ValueError as e:
         log(3, e)
         shutdown(save=False, abnormal=True)  # malformed data; terminate script
@@ -415,13 +416,13 @@ def main():
         ztp['serial'] = target.stack[sorted(target.stack.keys())[0]]
         # check if all specified switches are found, turn on beacon if not
         missing = set(target.stack.values()) - set(serials.values())
-        if len(missing):
+        if missing:
             log(4, 'Missing switch(es): %s' % ', '.join(missing))
             blue_beacon(serials.keys())
 
         # check if all found switches are specified, turn on beacon if not
         extra = set(serials.values()) - set(target.stack.values())
-        if len(extra):
+        if extra:
             log(4, 'Extra switch(es): %s' % ', '.join(extra))
             blue_beacon(serials.keys())
 
